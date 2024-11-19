8a89d371604aef1cc45a4d4a9e0c0e6e4ec7a0f8
simijosephInfy <simi.joesph@infosys.com>
2024-11-19 13:10:41 +0530

    Add files via upload

---
+++ BupaAustraliaAPI/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BupaAustraliaAPI.Middlewares;
+public class GlobalExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            ProblemDetails problem = new()
+            {
+                Status = (int)HttpStatusCode.InternalServerError,
+                Type = "Server error",
+                Title = "Server Error",
+                Detail = ex.Message
+            };
+            string json = JsonSerializer.Serialize(problem);
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
