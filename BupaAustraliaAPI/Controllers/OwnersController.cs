1c5283492da1e8d6a70b41d7e2b9598a55fe457a
simijosephInfy <simi.joesph@infosys.com>
2024-11-19 13:02:51 +0530

    Add files via upload

---
+++ BupaAustraliaAPI/Controllers/OwnersController.cs
@@ -0,0 +1,22 @@
+﻿using BupaAustraliaAPI.Interfaces;
+using BupaAustraliaAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BupaAustraliaAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OwnersController(IOwnersService ownersService) : ControllerBase
+{
+    [HttpGet("booksbycategory", Name = "GetBooksCategorizedByAge")]
+    public async Task<IActionResult> GetBooksCategorizedByAge()
+    {
+        var categorizedBooks = await ownersService.GetBooksCategorizedByAge();
+        if (categorizedBooks == null || !categorizedBooks.Any())
+        {
+            return NotFound("No categorized books found");
+        }
+        return Ok(await ownersService.GetBooksCategorizedByAge());
+    }
+
+}
