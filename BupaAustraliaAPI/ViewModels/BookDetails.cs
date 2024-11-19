e7b1f5d48e0463eb430426d7a1749249ca4983c0
simijosephInfy <simi.joesph@infosys.com>
2024-11-19 13:14:33 +0530

    Add files via upload

---
+++ BupaAustraliaAPI/ViewModels/BookDetails.cs
@@ -0,0 +1,7 @@
+﻿namespace BupaAustraliaAPI.ViewModels;
+public class BookDetails
+{
+    public string BookName { get; set; }
+    public string OwnerName { get; set; }
+    public int Age { get; set; }
+}
---
+++ BupaAustraliaAPI/ViewModels/CategorizedBooks.cs
@@ -0,0 +1,6 @@
+﻿namespace BupaAustraliaAPI.ViewModels;
+public class CategorizedBooks
+{
+    public string AgeCategory { get; set; }
+    public List<BookDetails> Book { get; set; }
+}
