91b15bdec3596526cfb5fe72b6a355d6295150d5
simijosephInfy <simi.joesph@infosys.com>
2024-11-19 13:13:25 +0530

    Add files via upload

---
+++ BupaAustraliaAPI/Services/ExternalApiService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using BupaAustraliaAPI.Configurations;
+using BupaAustraliaAPI.Interfaces;
+using BupaAustraliaAPI.Models;
+using Microsoft.Extensions.Options;
+
+namespace BupaAustraliaAPI.Services;
+public class ExternalApiService(HttpClient httpClient, IOptions<ExternalApiSettings> options) : IExternalApiService
+{
+    private readonly string endPointUrl = "api/v1/bookowners";
+    public async Task<IEnumerable<Owner>> GetBooksCategorizedByAge()
+    {
+        return await httpClient.GetFromJsonAsync<IEnumerable<Owner>>
+            ($"{options.Value.BaseUrl}/{endPointUrl}") ?? Enumerable.Empty<Owner>();
+    }
+}
---
+++ BupaAustraliaAPI/Services/OwnersService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BupaAustraliaAPI.Interfaces;
+using BupaAustraliaAPI.Models;
+using BupaAustraliaAPI.ViewModels;
+
+namespace BupaAustraliaAPI.Services;
+public class OwnersService(IExternalApiService externalApiService) : IOwnersService
+{
+    public async Task<IEnumerable<CategorizedBooks>> GetBooksCategorizedByAge()
+    {
+        return await CategorizeBookByAge(await externalApiService.GetBooksCategorizedByAge());
+    }
+    public Task<IEnumerable<CategorizedBooks>> CategorizeBookByAge(IEnumerable<Owner> owners)
+    {
+        var result = owners
+            .SelectMany(owner => owner.Books.Select(book => new
+            {
+                AgeCategory = owner.Age < 18 ? "Child" : "Adult",
+                BookDetails = new BookDetails
+                {
+                    BookName = book.Name,
+                    OwnerName = owner.Name,
+                    Age = owner.Age
+                }
+            }))
+            .GroupBy(x => x.AgeCategory)
+            .Select(group => new CategorizedBooks
+            {
+                AgeCategory = group.Key,
+                Book = group.Select(x => x.BookDetails).OrderBy(book => book.BookName).ToList()
+            });
+
+        return Task.FromResult(result.AsEnumerable());
+    }
+
+}
