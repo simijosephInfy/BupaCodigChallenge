ceb9586ab572f3e62e66687ac6c1159bb4697b8e
simijosephInfy <simi.joesph@infosys.com>
2024-11-22 15:48:28 +0530

    Add files via upload

--- BupaAustraliaAPI/Controllers/OwnersController.cs
+++ BupaAustraliaAPI/Controllers/OwnersController.cs
@@ -1,6 +1,7 @@
 ﻿using BupaAustraliaAPI.Interfaces;
 using BupaAustraliaAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace BupaAustraliaAPI.Controllers;
 
@@ -9,6 +10,9 @@ namespace BupaAustraliaAPI.Controllers;
 public class OwnersController(IOwnersService ownersService) : ControllerBase
 {
     [HttpGet("booksbycategory", Name = "GetBooksCategorizedByAge")]
+    [SwaggerOperation(Summary = "Gets books categorized by age", Description = "Returns a list of books categorized by the owner's age.")]
+    [SwaggerResponse(200, "Returns the list of categorized books", typeof(IEnumerable<CategorizedBooks>))]
+    [SwaggerResponse(404, "No categorized books found")]
     public async Task<IActionResult> GetBooksCategorizedByAge()
     {
         var categorizedBooks = await ownersService.GetBooksCategorizedByAge();
