655d79b4783e5841756479da8c089a444ffd9d3e
simijosephInfy <simi.joesph@infosys.com>
2024-11-22 15:47:37 +0530

    Add files via upload

--- BupaAustraliaAPI/Program.cs
+++ BupaAustraliaAPI/Program.cs
@@ -10,7 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Bupa API", Version = "v1" });
+    c.EnableAnnotations();
+});
 
 builder.Services.Configure<ExternalApiSettings>(
     builder.Configuration.GetSection("ExternalApiSettings"));
