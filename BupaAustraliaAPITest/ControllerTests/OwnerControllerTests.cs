6a77bd20fd0742111012eee8b07e629691f56721
simijosephInfy <simi.joesph@infosys.com>
2024-11-22 15:44:18 +0530

    Add files via upload

--- BupaAustraliaAPITest/ControllerTests/OwnerControllerTests.cs
+++ BupaAustraliaAPITest/ControllerTests/OwnerControllerTests.cs
@@ -5,67 +5,142 @@ using BupaAustraliaAPI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
-namespace BupaAustraliaAPITest.ControllerTests
+namespace BupaAustraliaAPITest.ControllerTests;
+public class OwnerControllerTests
 {
-    public class OwnerControllerTests
+    private Mock<IOwnersService> ownerServiceMock;
+    private OwnersController ownerController;
+
+    [SetUp]
+    public void SetUp()
     {
-        private Mock<IOwnersService> ownerServiceMock;
-        private OwnersController ownerController;
-
-        [SetUp]
-        public void SetUp()
-        {
-            ownerServiceMock = new Mock<IOwnersService>();
-            ownerController = new OwnersController(ownerServiceMock.Object);
-        }
-
-        [Test]
-        public async Task GetCategorizedBooks_ReturnsCategorizedBooks()
-        {
-            //Arrange
-            var categorizedBooks = new List<CategorizedBooks>
-            {
-            new CategorizedBooks
-            {
-                AgeCategory ="Child",
-                Book = new List<BookDetails>
-                {
-                    new BookDetails { BookName = "Book1", OwnerName = "Owner1", Age = 10 }
-                }
-            },
-            new CategorizedBooks
-            {
-                AgeCategory ="Adult",
-                Book = new List<BookDetails>
-                {
-                    new BookDetails { BookName = "Book2", OwnerName = "Owner1", Age = 20 }
-                }
-            }
-            };
-            ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
-                .ReturnsAsync(categorizedBooks);
-            //Act
-            var result = await ownerController.GetBooksCategorizedByAge();
-            //Assert
-            Assert.That(result, Is.InstanceOf<OkObjectResult>());
-            var okResult = result as OkObjectResult;
-            Assert.That(okResult, Is.Not.Null);
-            Assert.That(okResult.Value, Is.EqualTo(categorizedBooks));
-
-        }      
-        [Test]
-        public async Task GetBooksCategorizedByAge_ReturnsNotFound_WhenNoData()
-        {
-            ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
-                .ReturnsAsync(new List<CategorizedBooks>());
-            //Act
-            var result = await ownerController.GetBooksCategorizedByAge();
-            //Assert
-            Assert.That(result, Is.InstanceOf<NotFoundObjectResult>());
-            var notFoundResult = result as NotFoundObjectResult;
-            Assert.That(notFoundResult, Is.Not.Null);
-            Assert.That(notFoundResult.Value, Is.EqualTo("No categorized books found"));
-        }
-
+        ownerServiceMock = new Mock<IOwnersService>();
+        ownerController = new OwnersController(ownerServiceMock.Object);
     }
+
+    [Test]
+    public async Task GetCategorizedBooks_ReturnsCategorizedBooks()
+    {
+        //Arrange
+        var categorizedBooks = new List<CategorizedBooks>
+        {
+        new CategorizedBooks
+        {
+            AgeCategory ="Child",
+            Book = new List<BookDetails>
+            {
+                new BookDetails { BookName = "Book1", OwnerName = "Owner1", Age = 10 }
+            }
+        },
+        new CategorizedBooks
+        {
+            AgeCategory ="Adult",
+            Book = new List<BookDetails>
+            {
+                new BookDetails { BookName = "Book2", OwnerName = "Owner1", Age = 20 }
+            }
+        }
+        };
+        ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
+            .ReturnsAsync(categorizedBooks);
+        //Act
+        var result = await ownerController.GetBooksCategorizedByAge();
+        //Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult.Value, Is.EqualTo(categorizedBooks));
+    }
+    [Test]
+    public async Task GetBooksCategorizedByAge_CategorizesBooksCorrectly_ForMixedAgeCategories()
+    {
+        // Arrange
+        var categorizedBooks = new List<CategorizedBooks>
+    {
+        new CategorizedBooks
+        {
+            AgeCategory = "Child",
+            Book = new List<BookDetails>
+            {
+                new BookDetails { BookName = "Book1", OwnerName = "Owner1", Age = 10 }
+            }
+        },
+        new CategorizedBooks
+        {
+            AgeCategory = "Adult",
+            Book = new List<BookDetails>
+            {
+                new BookDetails { BookName = "Book2", OwnerName = "Owner2", Age = 20 }
+            }
+        }
+    };
+        ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
+                        .ReturnsAsync(categorizedBooks);
+
+        // Act
+        var result = await ownerController.GetBooksCategorizedByAge();
+
+        // Assert
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null);
+        Assert.That(okResult.Value, Is.EqualTo(categorizedBooks));
+
+        var returnedBooks = okResult.Value as List<CategorizedBooks>;
+        Assert.That(returnedBooks, Is.Not.Null);
+        Assert.That(returnedBooks.Count, Is.EqualTo(2));
+
+        var childCategory = returnedBooks.FirstOrDefault(c => c.AgeCategory == "Child");
+        Assert.That(childCategory, Is.Not.Null);
+        Assert.That(childCategory.Book.Count, Is.EqualTo(1));
+        Assert.That(childCategory.Book.First().BookName, Is.EqualTo("Book1"));
+        Assert.That(childCategory.Book.First().OwnerName, Is.EqualTo("Owner1"));
+        Assert.That(childCategory.Book.First().Age, Is.EqualTo(10));
+
+        var adultCategory = returnedBooks.FirstOrDefault(c => c.AgeCategory == "Adult");
+        Assert.That(adultCategory, Is.Not.Null);
+        Assert.That(adultCategory.Book.Count, Is.EqualTo(1));
+        Assert.That(adultCategory.Book.First().BookName, Is.EqualTo("Book2"));
+        Assert.That(adultCategory.Book.First().OwnerName, Is.EqualTo("Owner2"));
+        Assert.That(adultCategory.Book.First().Age, Is.EqualTo(20));
+    }
+    [Test]
+    public async Task GetBooksCategorizedByAge_ReturnsNotFound_WhenNoData()
+    {
+        ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
+            .ReturnsAsync(new List<CategorizedBooks>());
+        //Act
+        var result = await ownerController.GetBooksCategorizedByAge();
+        //Assert
+        Assert.That(result, Is.InstanceOf<NotFoundObjectResult>());
+        var notFoundResult = result as NotFoundObjectResult;
+        Assert.That(notFoundResult, Is.Not.Null);
+        Assert.That(notFoundResult.Value, Is.EqualTo("No categorized books found"));
+    }
+    [Test]
+    public async Task GetBooksCategorizedByAge_ReturnsNotFound_WhenReturnsNull()
+    {
+        ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
+                          .ReturnsAsync((IEnumerable<CategorizedBooks>)null);
+        //Act
+        var result = await ownerController.GetBooksCategorizedByAge();
+        //Assert
+        Assert.That(result, Is.InstanceOf<NotFoundObjectResult>());
+        var notFoundResult = result as NotFoundObjectResult;
+        Assert.That(notFoundResult, Is.Not.Null);
+        Assert.That(notFoundResult.Value, Is.EqualTo("No categorized books found"));
+    }
+    public async Task GetBooksCategorizedByAge_ReturnsNotFound_WhenReturnsEmpty()
+    {
+        ownerServiceMock.Setup(service => service.GetBooksCategorizedByAge())
+                          .ReturnsAsync(Enumerable.Empty<CategorizedBooks>);
+        //Act
+        var result = await ownerController.GetBooksCategorizedByAge();
+        //Assert
+        Assert.That(result, Is.InstanceOf<NotFoundObjectResult>());
+        var notFoundResult = result as NotFoundObjectResult;
+        Assert.That(notFoundResult, Is.Not.Null);
+        Assert.That(notFoundResult.Value, Is.EqualTo("No categorized books found"));
+    }
+
 }
